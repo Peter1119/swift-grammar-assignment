74ce5482a53c3765ee7e9d03f2206bfd4bf39f9a
홍석현 <ghdtjrgus333@naver.com>
2025-08-21 13:55:05 +0900

    feat. for-in 문을 map 을 사용하는 코드로 변환해주세요.

---
+++ SwiftBasicAssignment/SwiftBasicAssignment/필수과제2/assignment2.swift
@@ -0,0 +1,19 @@
+//
+//  assignment2.swift
+//  SwiftBasicAssignment
+//
+//  Created by 홍석현 on 8/21/25.
+//
+
+import Foundation
+
+
+// 1. map 으로 변환
+func convertToString() {
+    let result = [1, 2, 3, 4, 5].map { String($0) }
+    print(result)
+}
+
+
+
+
