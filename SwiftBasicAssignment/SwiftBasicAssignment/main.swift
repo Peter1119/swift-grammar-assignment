ec2d23e570153cc90c92a1ffafa3e8804c4b84fc
홍석현 <ghdtjrgus333@naver.com>
2025-08-21 11:11:35 +0900

    feat. 과제 1번

---
+++ SwiftBasicAssignment/SwiftBasicAssignment/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  SwiftBasicAssignment
+//
+//  Created by 홍석현 on 8/20/25.
+//
+
+import Foundation
+
+print(sum(10, 20))
+
+calculate(10, b: 20, sum: { a, b in
+    return "두 수의 합은 \(a + b)입니다."
+})
+
---
+++ SwiftBasicAssignment/SwiftBasicAssignment/필수과제1/assignment1.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1.swift
+//  SwiftBasicAssignment
+//
+//  Created by 홍석현 on 8/21/25.
+//
+
+import Foundation
+
+let sum: (Int, Int) -> String = { a, b in
+    return "두 수의 합은 \(a + b)입니다."
+}
+
+//
+func calculate(_ a: Int, b: Int, sum: ((Int, Int) -> String)) {
+    let _ = sum(a, b)
+}
