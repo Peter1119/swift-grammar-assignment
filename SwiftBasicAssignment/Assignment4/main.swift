0dd665e0686c7e133276d4bbc6f571bd5f571d29
홍석현 <ghdtjrgus333@naver.com>
2025-08-21 15:58:28 +0900

    feat. [Introducible] 타입 배열을 정의 및 배열을 순회하며 각 타입 고유의 메서드들을 호출하는 코드를 작성해

--- SwiftBasicAssignment/Assignment4/main.swift
+++ SwiftBasicAssignment/Assignment4/main.swift
@@ -7,19 +7,40 @@
 
 import Foundation
 
-var robot = Robot(name: "피규어")
-robot.name = "옵티머스"
-print(robot.introduce())
+//var robot = Robot(name: "피규어")
+//robot.name = "옵티머스"
+//print(robot.introduce())
+//
+//robot.move()
+//robot.move()
+//
+//for _ in 0..<10 {
+//    robot.move()
+//}
+//
+//await robot.charge()
+//
+//for _ in 0..<10 {
+//    robot.move()
+//}
 
-robot.move()
-robot.move()
+let robot = Robot(name: "아이언맨")
+let cat = Cat(name: "고양이")
+let dog = Dog(name: "강아지")
 
-for _ in 0..<10 {
-    robot.move()
-}
+var introducibles = [Introducible]()
+introducibles.append(robot)
+introducibles.append(cat)
+introducibles.append(dog)
 
-await robot.charge()
-
-for _ in 0..<10 {
-    robot.move()
+for introducible in introducibles {
+    print(introducible.introduce())
+    
+    if let cat = introducible as? Cat {
+        print(cat.meow())
+    } else if let dog = introducible as? Dog {
+        print(dog.bark())
+    } else if var robot = introducible as? Robot {
+        robot.move()
+    }
 }
