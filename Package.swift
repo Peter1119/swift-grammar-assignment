2378fd86c3ae094a956b540a370e3494012f5db3
홍석현 <ghdtjrgus333@naver.com>
2025-08-22 21:59:12 +0900

    feat. SPM 으로 변경
    과제 마다 target을 만들면 project.swift 에서 충돌이 발생하고 해결하기 어려움

---
+++ Package.swift
@@ -0,0 +1,21 @@
+
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftBasicAssignment",
+    platforms: [
+        .macOS(.v13)  // macOS 13.0 이상 요구
+    ],
+    products: [
+        .executable(name: "Assignment1", targets: ["Assignment1"]),
+        .executable(name: "Assignment4", targets: ["Assignment4"]),
+        .executable(name: "Assignment5", targets: ["Assignment5"]),
+    ],
+    targets: [
+        .executableTarget(name: "Assignment1", path: "Sources/Assignment1"),
+        .executableTarget(name: "Assignment4", path: "Sources/Assignment4"),
+        .executableTarget(name: "Assignment5", path: "Sources/Assignment5"),
+    ]
+)
