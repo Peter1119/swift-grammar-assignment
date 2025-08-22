85eea7610aaf1e6418f8895a37a21212385d64d7
홍석현 <ghdtjrgus333@naver.com>
2025-08-22 10:09:46 +0900

    feat. DeliveryService 객체 생성 및 구현

--- SwiftBasicAssignment/Assignment5/DeliveryService.swift
+++ SwiftBasicAssignment/Assignment5/DeliveryService.swift
@@ -7,3 +7,32 @@
 
 import Foundation
 
+public struct DeliveryService {
+    public func predictDeliveryDay(
+        for address: String,
+        status: DeliveryStatus
+    ) throws -> String {
+        try validateAddress(address)
+        
+        switch status {
+        case .notStarted:
+            throw DeliveryError.notStarted
+        case .inTransit(let daysRemaining):
+            return "배송까지 \(daysRemaining)일 남았습니다."
+        case .error:
+            throw DeliveryError.systemError(reason: "배송 정보를 찾을 수 없습니다.")
+        }
+    }
+}
+
+extension DeliveryService {
+    private func validateAddress(
+        _ address: String
+    ) throws {
+        if address.isEmpty {
+            throw DeliveryError.invalidAddress
+        } else {
+            return
+        }
+    }
+}
