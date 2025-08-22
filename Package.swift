d0a1de3fc48cf2b0bdba81a5e348fc7d077ea693
홍석현 <ghdtjrgus333@naver.com>
2025-08-22 22:08:33 +0900

    chor. 필수 과제 이름 변경

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         .macOS(.v13)  // macOS 13.0 이상 요구
     ],
     products: [
-        .executable(name: "Assignment1", targets: ["Assignment1"]),
+        .executable(name: "Assignment1-3", targets: ["Assignment1-3"]),
         .executable(name: "Assignment4", targets: ["Assignment4"]),
         .executable(name: "Assignment5", targets: ["Assignment5"]),
         .executable(name: "Challenge1", targets: ["Challenge1"]),
     ],
     targets: [
-        .executableTarget(name: "Assignment1", path: "Sources/Assignment1"),
+        .executableTarget(name: "Assignment1-3", path: "Sources/Assignment1-3"),
         .executableTarget(name: "Assignment4", path: "Sources/Assignment4"),
         .executableTarget(name: "Assignment5", path: "Sources/Assignment5"),
         .executableTarget(name: "Challenge1", path: "Sources/Challenge1"),
