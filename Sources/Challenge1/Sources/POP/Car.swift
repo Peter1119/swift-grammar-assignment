cac298e1459ff350cff02a3edd2b1965cd83e5fe
홍석현 <ghdtjrgus333@naver.com>
2025-08-24 15:26:12 +0900

    feat. Car 객체 생성

--- Sources/Challenge1/Sources/POP/Car.swift
+++ Sources/Challenge1/Sources/POP/Car.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+struct Car: Drivable {
+    let brand: String
+    let model: String
+    let year: Int
+    let engine: Engine
+
+    init(
+        brand: String,
+        model: String,
+        year: Int,
+        engine: Engine
+    ) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine = engine
+    }
+    
+    func drive() {
+        print("Car가 움직입니다.")
+    }
+    
+    func parking() {
+        print("Car가 주차합니다.")
+    }
+    
+    func refuel() {
+        print("Car가 연료가 충전됩니다.")
+    }
+    
+}
