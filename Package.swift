d7b239029531114c05c87acb3976234ac15039c5
홍석현 <78390837+Peter1119@users.noreply.github.com>
2025-08-25 10:09:50 +0900

    [도전 과제 2] 제네릭 구조체 #7 (#17)
    * feat. SortableBox 및 메서드 구현
    
    * chor. 버그 수정

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .executable(name: "Assignment5", targets: ["Assignment5"]),
         .executable(name: "Challenge1-OOP", targets: ["Challenge1-OOP"]),
         .executable(name: "Challenge1-POP", targets: ["Challenge1-POP"]),
+        .executable(name: "Challenge2", targets: ["Challenge2"]),
     ],
     targets: [
         .executableTarget(name: "Assignment1-3", path: "Sources/Assignment1-3"),
@@ -27,5 +28,6 @@ let package = Package(
             name: "Challenge1-POP", 
             path: "Sources/Challenge1/Sources/POP"
         ),
+        .executableTarget(name: "Challenge2", path: "Sources/Challenge2"),
     ]
 )
